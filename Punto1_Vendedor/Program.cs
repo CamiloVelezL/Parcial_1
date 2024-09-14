038fcee682c9ad8dadba28c83322b26e8433bb38
CamiloVelezL <105832950+CamiloVelezL@users.noreply.github.com>
2024-09-13 20:39:42 -0500

    Primer Commit parcial vendedor

---
+++ Punto1_Vendedor/Program.cs
@@ -0,0 +1,23 @@
+﻿//un vendedor recibe un suelto base de $600.000. ademas recibe una comision del 10% sobre el valor total de cada venta que realiza.
+
+//el vendedor desea saber:
+//¿cuanto dinero recibira por el concepto de comisiones por las tres ventas que realiza en el mes ?
+//¿cual es el total que recibira en el mes tomando en cuenta su sueldo base y comisiones ?
+//¿cual fue la venta que le genero la mayor comision ?
+//¿cual fue el promedio de las comisones que recibio por cada venta ?
+
+//Ademas la empresa da un beneficio extra de $100.000 si el vendedor supera el objetivo del mes.
+// El objetivo es sumar en las 3 ventas como minimo $1.000.000 en ventas.
+//Mostrar un mensaje donde se indique si gano o no el beneficio.
+
+public class Program
+{      
+    private static void Main(string[] args)
+    {
+        //Delcaración Datos fijos 
+        decimal SueldoBase = 900000;
+        decimal Comision = 10 %;
+        decimal BeneficioExtra = 100000;
+        decimal ObjetivoVentas = 1000000;
+    }
+}
