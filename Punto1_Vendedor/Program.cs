36839a0465467f1f099cacfbb42366e716a05215
CamiloVelezL <105832950+CamiloVelezL@users.noreply.github.com>
2024-09-13 21:38:33 -0500

    Orden de Salidas Final

--- Punto1_Vendedor/Program.cs
+++ Punto1_Vendedor/Program.cs
@@ -10,8 +10,11 @@
 // El objetivo es sumar en las 3 ventas como minimo $1.000.000 en ventas.
 //Mostrar un mensaje donde se indique si gano o no el beneficio.
 
+using System;
+using System.ComponentModel.Design;
+
 public class Program
-{      
+{
     private static void Main(string[] args)
     {
         //Delcaración Datos fijos 
@@ -23,7 +26,7 @@ public class Program
         //Declaracion de variable Ventas
         decimal VentaNo1;
         Console.Write("Ingrese el valor de la primera venta: ");
-        VentaNo1=Convert.ToInt32(Console.ReadLine());
+        VentaNo1 = Convert.ToInt32(Console.ReadLine());
 
         decimal VentaNo2;
         Console.Write("Ingrese el valor de la segunda venta: ");
@@ -34,9 +37,9 @@ public class Program
         VentaNo3 = Convert.ToInt32(Console.ReadLine());
 
         //comision de ventas
-        decimal Comision1= VentaNo1 * Comision;
-        decimal Comision2= VentaNo2 * Comision;
-        decimal Comision3= VentaNo3 * Comision;
+        decimal Comision1 = VentaNo1 * Comision;
+        decimal Comision2 = VentaNo2 * Comision;
+        decimal Comision3 = VentaNo3 * Comision;
 
         //Total de comision
         decimal TotalComision = Comision1 + Comision2 + Comision3;
@@ -45,14 +48,32 @@ public class Program
         decimal TotalaRecibir = SueldoBase + TotalComision;
 
         //Venta de mayor comision
-        decimal VentaMayor = Math.Max(Comision1,Math.Max(Comision2,Comision3));
+        decimal VentaMayor = Math.Max(Comision1, Math.Max(Comision2, Comision3));
 
         //promedio de comisiones
 
         decimal PromedioComsiones = TotalComision / 3;
 
         //Analisis de Cumplimiento
-        decimal TotalVentas =VentaNo1 + VentaNo2 + VentaNo3;
+        decimal TotalVentas = VentaNo1 + VentaNo2 + VentaNo3;
         Boolean ObjetivoAlcanzado = TotalVentas >= ObjetivoVentas;
+
+        //Resultados del trabajador
+
+        Console.WriteLine($"El total de Comsiones del mes es: {TotalComision}");
+        Console.WriteLine($"El total de sueldo y Comsiones es: {TotalaRecibir}");
+        Console.WriteLine($"Su mayor Comsion del mes fue: {VentaMayor}");
+        Console.WriteLine($"Su promedio de Comsiones del mes fue: {PromedioComsiones}");
+
+        if (ObjetivoAlcanzado)
+        {
+            Console.WriteLine($"!!!! Felicidades ¡¡¡¡ Vendio mas de 1.000.000 y recibira el beneficio extra de {BeneficioExtra}");
+            TotalaRecibir += BeneficioExtra;
+            Console.WriteLine($"Este mes Has Ganado en Total: {TotalaRecibir}");
+        }
+        else
+        {
+            Console.WriteLine("Este Mes no alcanzaste el Objetivo de Ventas, no tendras el beneficio Extra :( ");
+        }
     }
 }
